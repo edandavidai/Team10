d6a72f56d52ef2b18338e4ce4c3157b4648291e1
edandavidai <75982389+edandavidai@users.noreply.github.com>
2022-02-16 13:58:04 +0200

    Delete Matching_Algorithm.py

--- appApp/src/components/Matching_Algorithm.py
+++
@@ -1,8 +0,0 @@
-"""
-Description: takes two lists of requests from organizations and from units and pairs them using a weighted maximal matching algorithm. Weights are based on both priority and number of people provided 
-Parameters: org_requests - table with columns ID, ORG, NUM_PEOPLE, DATE
-            unit_requests - table with ID, UNIT, ORG_ID, PRIORITY
-            
-Returns: list of pairs (org_requests_id, unit_requests_id)
-            
-"""    
